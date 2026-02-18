ddc54bc16e0ffadc60c35fa03d5c95f30cc640ec
Shaik Nazeer <shaiknazeer0073@email.com>
2026-02-18 10:32:46 +0530

    Added authentication routes and connected to server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
 
 dotenv.config();
 
 const app = express();
-
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 app.use(cors());
 app.use(express.json());
 
