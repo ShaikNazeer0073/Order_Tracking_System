beb15d9fa2f65a4092e19ec56fa9a46c096c9732
Shaik Nazeer <shaiknazeer0073@email.com>
2026-02-18 11:01:53 +0530

    Create Order model

---
+++ backend/models/Order.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Shipped", "Delivered"],
+      default: "Pending",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
